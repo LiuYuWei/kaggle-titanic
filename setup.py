2087c21a611863ca45ff8c9e402f0b80920d5405
yuweiliu <yuweiliu@fareastone.com.tw>
2020-07-13 14:59:04 +0800

    Release 0.2.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     project_name='kaggle-titanic',
-    project_version='0.2.4',
+    project_version='0.2.5',
     description='In this project, we use machine learning to predict the classification data.',
     long_description=readme_text,
     author='Simon Liu',
